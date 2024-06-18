024f985e1d22b8e16698930dafd9a69245092cdb
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-06-18 13:17:56 +0530

    modify file

--- server/app.js
+++ server/app.js
@@ -69,7 +69,6 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
-//app.use('/profile-pictures', express.static('./tmp'));
 app.use('/profile-pictures', express.static(path.join(__dirname, 'public/profile-pictures')));
 app.use(cookieParser());
 
