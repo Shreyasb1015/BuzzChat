3c9e852cb8cc836f95ba348edd3a582a9ac410a0
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-06-18 14:44:07 +0530

    fixed minor bug

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,6 +4,12 @@ import {ApiError} from '../utils/ApiError.js'
 import {User} from '../models/user.models.js'
 import fs from 'fs'
 import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const cookieOptions={
     httpOnly:true,
