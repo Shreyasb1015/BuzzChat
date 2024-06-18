61ad13993c2e7df8d9f2ef5a706db442b2d94cec
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-06-18 13:45:18 +0530

    modified files

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "https://buzzchat-backend.onrender.com";
 
 export const socket=io(URL,{
     withCredentials:true,
--- server/app.js
+++ server/app.js
@@ -66,7 +66,10 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true,
 }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use('/profile-pictures', express.static(path.join(__dirname, 'public/profile-pictures')));
